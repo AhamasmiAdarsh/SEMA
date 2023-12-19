5c2de34168c63bde423e8d216befddfa9f6a6a72
Ahamasmiadarsh <adarshtiwari.singer@gmail.com>
2023-12-19 15:33:46 +0530

    semi home (almost)

--- app/src/main/java/com/example/sema3/MainActivity.java
+++ app/src/main/java/com/example/sema3/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import com.example.sema3.Fragment.AddFragment;
 import com.example.sema3.Fragment.CommunityFragment;
 import com.example.sema3.Fragment.HomeFragment;
+import com.example.sema3.Fragment.ShopFragment;
 import com.example.sema3.Fragment.UsageFragment;
 import com.example.sema3.Fragment.UserFragment;
 
@@ -45,23 +46,19 @@ public class MainActivity extends AppCompatActivity {
                 switch (i){
                     case 0 :
                         transaction.replace(R.id.container, new HomeFragment());
-                        Toast.makeText(MainActivity.this, "Home Selected", Toast.LENGTH_SHORT).show();
                         break;
                     case 1 :
                         transaction.replace(R.id.container, new UsageFragment());
-                        Toast.makeText(MainActivity.this, "Search Selected", Toast.LENGTH_SHORT).show();
                         break;
                     case 2 :
                         transaction.replace(R.id.container, new AddFragment());
-                        Toast.makeText(MainActivity.this, "Add Selected", Toast.LENGTH_SHORT).show();
                         break;
                     case 3 :
                         transaction.replace(R.id.container, new CommunityFragment());
-                        Toast.makeText(MainActivity.this, "Community Tab Selected", Toast.LENGTH_SHORT).show();
                         break;
                     case 4 :
                         transaction.replace(R.id.container, new UserFragment());
-                        Toast.makeText(MainActivity.this, "User Selected", Toast.LENGTH_SHORT).show();
+
                         break;
 
                 }
